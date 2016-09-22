764f1c84c845f16ad4a5085fbf6b370e5a4e3626
Renee Yun <reneeyun40@yahoo.com>
2016-09-22 00:18:07 -0700

    fixed problems 4 and 5

--- Pinter-Wollman_Day2_HW.R
+++ Pinter-Wollman_Day2_HW.R
@@ -12,13 +12,15 @@ data <- within (data, {
   Time <- factor(Time)
 })
 
+
 #Example 1
 data.t0 <- data[data$Time==0,]
 #Naming this dataset, all rows where time=0 to be data.t0
 windows()
-boxplot(weight~Diet, data=data.t0, xlab= "Diet Type", ylab= "Weight", main= "Weight of Chicks vs. Diet Treatments- Initial, Day 0", 
+boxplot(weight~Diet, data=ChickWeight.t0, xlab= "Diet Type", ylab= "Weight", main= "Weight of Chicks vs. Diet Treatments- Initial, Day 0", 
         las=1,
         cex.lab= 1.5)
+
 anova.t0 <-aov(weight~Diet, data=data.t0)
 summary(anova.t0)
 #weight by diet on day 0 is not significant, p=0.346
@@ -45,19 +47,31 @@ summary(aov(weight~Diet*Time+Error(Chick),data=data))
 
 #Example 4
 windows()
-plot(as.numeric(data$Time), data$weight, pch=20)
+plot(as.numeric(ChickWeight$Time), ChickWeight$weight, pch=20)
+#plot weight of chicks as time passes, with each diet type being a different color
 
-cols= sub('1', "cyan", data$Diet)
+cols= sub('1', "cyan", ChickWeight$Diet)
 cols= sub('2', "chartreuse", cols)
 cols= sub('3', "gold", cols)
 cols= sub('4', "hotpink", cols)
+#define colors for each diet type
+#cols2 <- c("cyan", "chartreuse", "gold", "hotpink")[unclass(data$Diet)] would be an alternative way
 
-plot(as.numeric(data$Time), data$weight,
+plot(as.numeric(ChickWeight$Time), ChickWeight$weight,
      xlab= "Time", ylab= "Weight", main= "Effect of Diet on Chick Weight over Time",
      col=cols,
      pch= 20, las= 1, cex.lab= 1.5)
 
-legend("topleft", title="Diet Treatment", legend=unique(data$Diet), text.col=c("cyan", "chartreuse", "gold", "hotpink"), pch=20, col=c("cyan", "chartreuse", "gold", "hotpink"))
+legend("topleft", title="Diet Treatment", legend=unique(ChickWeight$Diet), text.col=c("cyan", "chartreuse", "gold", "hotpink"), pch=20, col=c("cyan", "chartreuse", "gold", "hotpink"))
+
+#alternatively
+windows()
+cols2 <- c("cyan", "chartreuse", "gold", "hotpink")[unclass(ChickWeight$Diet)] 
+plot(as.numeric(ChickWeight$Time), ChickWeight$weight,
+     main= "Effect of Diet on Chick Weight over Time", xlab= "Time", ylab="Weight",
+     col=cols2,
+     pch=20, las=1, cex.lab=1.5)
+legend("topleft", title="Diet Treatment", legend=unique(ChickWeight$Diet), text.col=c("cyan", "chartreuse", "gold", "hotpink"), pch=20, col=c("cyan", "chartreuse", "gold", "hotpink"))
 
 
 #Example 5
@@ -68,8 +82,9 @@ plot(NA, ylim=c(0,370), xlim=c(0,21),
 colors=rainbow(50)
 
 for(n in 1:50){
-  use.rows=which(data$Chick==n)
-  lines(x=data$Time[use.rows],
-        y=data$weight[use.rows],col=colors[n])
+  use.rows=which(ChickWeight$Chick==n)
+  lines(x=ChickWeight$Time[use.rows],
+        y=ChickWeight$weight[use.rows],col=colors[n])
 }
+
 
