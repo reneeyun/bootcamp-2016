10a2b2fa1d1793de92e97e9faf26d63c975506c0
Renee Yun <reneeyun40@yahoo.com>
2016-09-22 23:03:29 -0700

    finally done

--- Lohmueller_Day2_HW.R
+++ Lohmueller_Day2_HW.R
@@ -42,7 +42,33 @@ num_pval <- length(pvals) ##4014 SNPs
 #Part d
 exp_pvals <- (seq(1,num_pval,by=1))/num_pval ##numerator goes from 1 to num_pval (which is 4014), denominator stays same
 
+#Part e
+sort_pvals <- sort(pvals, decreasing=FALSE)
 
+#Part f
+log_sort_pvals <- (-log10(sort_pvals))
+log_exp_pvals <- (-log10(exp_pvals))
 
+#Part g
+plot(log_exp_pvals, log_sort_pvals,
+     xlab = "-log10(expected P-value)", ylab = "-log10(observed P-value)",
+     pch=19)
 
+#Part h
+abline(0, 1, h=5, col=2, lty=2) #0 intercept, slope=1
 
+##Question 2, Part a
+zz <- read.table("pheno.sim.2014-1.txt", header=TRUE)
+
+#Part b
+quantile(zz$glucose_mmolperL, 0.25)
+
+#Part c
+quantile(zz$glucose_mmolperL, 0.75)
+
+#Part d
+hist(zz$glucose_mmolperL, xlab="glucose mm/L", main="Histogram", las=1)
+lowtail <- quantile(zz$glucose_mmolperL, 0.25)
+hightail <- quantile(zz$glucose_mmolperL, 0.75)
+abline(v=lowtail, col=4)
+abline(v=hightail, col=2)
